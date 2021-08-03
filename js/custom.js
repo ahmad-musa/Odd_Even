78e18ff07da8330229b03fe8d54c1c9b321efa7e
ahmad-musa <ahmadmusa0987@gmail.com>
2021-08-03 21:02:32 +0600

    clicked css & js all ok

--- js/custom.js
+++ js/custom.js
@@ -14,6 +14,39 @@
 // })
 
 
+let nav_trigger = document.getElementById('nav_trigger');
+let nav_close = document.getElementById('close');
+
+nav_trigger.addEventListener('click',function(e){
+    e.preventDefault();
+    document.getElementById('navbar1').classList.toggle('navbar_show');
+    document.getElementById('navbar2').classList.remove('navbar_show2');
+});
+
+nav_close.addEventListener('click',function(e){
+    e.preventDefault();
+    document.getElementById('navbar1').classList.remove('navbar_show');
+});
+
+
+let nav_trigger2 = document.getElementById('nav_trigger2');
+let nav_close2 = document.getElementById('close2');
+
+nav_trigger2.addEventListener('click',function(e){
+    e.preventDefault();
+    document.getElementById('navbar2').classList.toggle('navbar_show2');
+    document.getElementById('navbar1').classList.remove('navbar_show');
+});
+
+nav_close2.addEventListener('click',function(e){
+    e.preventDefault();
+    document.getElementById('navbar2').classList.remove('navbar_show2');
+});
+
+
+
+
+
 ///////////////       Result                      ////////////////////
 
 // Output Show Function // 
